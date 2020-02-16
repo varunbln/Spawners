fc471b3d8873964f0e7bc5ee05c4802af72e5428
provsalt <39023494+provsalt@users.noreply.github.com>
2020-02-16 15:18:47 +0000

    Add ConfigUpdater

--- src/Heisenburger69/BurgerSpawners/Main.php
+++ src/Heisenburger69/BurgerSpawners/Main.php
@@ -25,6 +25,8 @@ use JackMD\ConfigNotifier\ConfigUpdater;
 class Main extends PluginBase
 {
 
+	/** @var int  */
+	private const CFGVERSION = 1;
     /** @var string */
     public const PREFIX = C::BOLD . C::AQUA . "Burger" . C::LIGHT_PURPLE . "Spawners" . "> " . C::RESET;
 
@@ -49,6 +51,7 @@ class Main extends PluginBase
         if(ConfigManager::getToggle("register-mobs")) {
             EntityManager::init();
         }
+        ConfigUpdater::checkUpdate($this, $this->getConfig(), "config-ver", self::CFGVERSION);
 
         UpdateNotifier::checkUpdate($this, $this->getDescription()->getName(), $this->getDescription()->getVersion());
     }
