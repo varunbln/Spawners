638812d4759827dc9714aa4eaeefc03f2c100422
VpsOP <57450981+VpsOP@users.noreply.github.com>
2020-02-18 13:06:19 +0530

    Update MobSpawnerTile.php

--- src/Heisenburger69/BurgerSpawners/Tiles/MobSpawnerTile.php
+++ src/Heisenburger69/BurgerSpawners/Tiles/MobSpawnerTile.php
@@ -306,8 +306,9 @@ class MobSpawnerTile extends Spawnable
     {
         Forms::sendAddSpawnerForm($player, $this);
     }
-    public function sendRemoveSpawnersForm(Player $player): void
-    {
-        Forms::sendRemoveSpawnerForm($player, $this);
-    }
+
+    
+    
+    
 }
+
