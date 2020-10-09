12423fe660d6c139966329239bfc680b305793fc
Heisenburger69 <apkforgss@gmail.com>
2020-10-09 18:32:06 +0530

    Update SpawnerEvent.php

--- src/Heisenburger69/BurgerSpawners/Event/SpawnerEvent.php
+++ src/Heisenburger69/BurgerSpawners/Event/SpawnerEvent.php
@@ -4,6 +4,7 @@ namespace Heisenburger69\BurgerSpawners\Events;
 
 use Heisenburger69\BurgerSpawners\Tiles\MobSpawnerTile;
 use pocketmine\event\Event;
+use pocketmine\Player;
 
 class SpawnerEvent extends Event
 {
@@ -11,9 +12,14 @@ class SpawnerEvent extends Event
      * @var MobSpawnerTile
      */
     private $spawnerTile;
+    /**
+     * @var Player
+     */
+    private $player;
 
-    public function __construct(MobSpawnerTile $spawnerTile)
+    public function __construct(Player $player, MobSpawnerTile $spawnerTile)
     {
+        $this->player = $player;
         $this->spawnerTile = $spawnerTile;
     }
 
@@ -23,5 +29,13 @@ class SpawnerEvent extends Event
     public function getSpawnerTile(): MobSpawnerTile
     {
         return $this->spawnerTile;
+    }
+
+    /**
+     * @return Player
+     */
+    public function getPlayer(): Player
+    {
+        return $this->player;
     }
 }
