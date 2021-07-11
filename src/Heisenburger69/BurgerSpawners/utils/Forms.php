2e999903cfcace75cb472057277228f0f2fa2ae7
Heisenburger69 <apkforgss@gmail.com>
2021-07-12 01:27:25 +0530

    Fix #109

--- src/Heisenburger69/BurgerSpawners/utils/Forms.php
+++ src/Heisenburger69/BurgerSpawners/utils/Forms.php
@@ -142,6 +142,7 @@ class Forms
 
                     $entityId = $spawner->getEntityId();
                     $count = (int)$response[1];
+                    if($count <= 0) return;
                     $max = $spawner->getCount();
 
                     $message = TextFormat::GREEN . "Removed spawners!";
